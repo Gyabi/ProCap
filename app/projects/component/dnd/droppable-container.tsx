cf2c774b2e81998fe2d1a747dd8639f4d86cbe37
Gyabi <buyuuka12345@gmail.com>
2024-01-28 22:34:47 +0900

    削除機能実装

--- app/projects/component/dnd/droppable-container.tsx
+++ app/projects/component/dnd/droppable-container.tsx
@@ -6,6 +6,7 @@ import { RxDragHandleDots2 } from "react-icons/rx";
 import { useEffect, useState } from "react";
 import { CancelButton } from "../buttons/cancel-button";
 import { EditButton } from "../buttons/edit-button";
+import { ask } from "@tauri-apps/api/dialog";
 
 const animateLayoutChanges: AnimateLayoutChanges = (args) =>
   defaultAnimateLayoutChanges({...args, wasDragging: true});
@@ -47,12 +48,10 @@ export const DroppableContainer: React.FC<DroppableContainerProps> = ({ id, chil
   const [isEditing, setIsEditing] = useState(false);
   const [editingText, setEditingText] = useState(title);
 
-  // useEffect(() => {
-  // }, [title]);
-
-    return (
-      <div ref={setNodeRef} style={style} className="relative w-full p-2 mb-2 flex flex-col bg-white border border-2 border-gray-200 hover:border-purple-800  rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700">
-          <div className="flex justify-start">
+  return (
+    <div ref={setNodeRef} style={style} className="relative w-full p-2 mb-2 flex flex-col bg-white border border-2 border-gray-200 hover:border-purple-800  rounded-xl shadow-lg dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex justify-between w-full">
+          <div className="flex justify-start w-full">
             {!isEditing ?
             <h5 className="mb-2 text-2xl font-bold tracking-tight decoration-amber-700 underline text-gray-900 dark:text-white" style={{ wordWrap: 'break-word' }}>{title}</h5>
             :
@@ -62,7 +61,7 @@ export const DroppableContainer: React.FC<DroppableContainerProps> = ({ id, chil
             }
             <div className="mx-2">
               <EditButton onClick={()=> setIsEditing((state)=>{
-                if(state){
+                if(state && editingText !== title){
                   // コンテナ名の更新
                   updateContainer(title, editingText);
                 }
@@ -70,12 +69,32 @@ export const DroppableContainer: React.FC<DroppableContainerProps> = ({ id, chil
               })} />
             </div>
           </div>
-          {children}
-          <div className="absolute top-4 right-4">
-              <button {...listeners} {...attributes}>
-                  <RxDragHandleDots2 size={24} />
-              </button>
+          {isEditing &&
+          <div className="mr-10">
+            <CancelButton onClick={async ()=>{
+              await ask("本当に削除しますか？",{
+                title: "",
+                type: "warning",
+                okLabel: "OK",
+                cancelLabel: "Cancel",
+              }).then(async (result) => {
+                if(result){
+                  setIsEditing(false);
+                  // データの削除と保存を実行
+                  deleteContainer(title);
+                };
+              });
+            }}
+            />
           </div>
-      </div>
-    );
+          }
+        </div>
+        {children}
+        <div className="absolute top-4 right-4">
+            <button {...listeners} {...attributes}>
+                <RxDragHandleDots2 size={24} />
+            </button>
+        </div>
+    </div>
+  );
 }
